e0e359b8755be6646b229d039a3a4ea03d743051
Leonardo Rosero <Leonardo.Rosero@fundacion-jala.org>
2023-06-13 18:28:49 -0500

    Act 1
    Corrección dada en  clases

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,8 +1,9 @@
 import pygame
 from pygame.sprite import Sprite
 
-from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import SPACESHIP, SCREEN_HEIGHT
 
+MOV = 7 # velocidad de Movimiento de la Nave por el momento es una constante 
 class SpaceShip(Sprite):
     def __init__(self):
         self.image_size = (40, 60)
@@ -15,14 +16,14 @@ class SpaceShip(Sprite):
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:  # Tecla A para moverse a la izquierda
-            self.image_rect.x -= 7
+            self.image_rect.x -= MOV
             if self.image_rect.right < 0:
                 self.image_rect.left = self.screen_width
         if keys[pygame.K_d]:  # Tecla D para moverse a la derecha
-            self.image_rect.x += 7
+            self.image_rect.x += MOV
             if self.image_rect.left > self.screen_width:
                 self.image_rect.right = 0
         if keys[pygame.K_w] and self.image_rect.top > 0:  # Tecla W para moverse hacia arriba
-            self.image_rect.y -= 7
+            self.image_rect.y -= MOV
         if keys[pygame.K_s] and self.image_rect.bottom < SCREEN_HEIGHT:  # Tecla S para moverse hacia abajo
-            self.image_rect.y += 7
+            self.image_rect.y += MOV
