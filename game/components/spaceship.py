d287c770b62ac536abbb68fc94e5dedee3a92e12
Leonardo Rosero <Leonardo.Rosero@fundacion-jala.org>
2023-06-12 19:55:59 -0500

    Actividad 1
    Se realizo el Ajuste para que la Nave pueda moverse utilizando las teclas AWSD respectivamente para "A" para moverse a la izquierda, "W" para moverse al frente , "S" para moverse hacia atrás y  "D"  para moverse  a la derecha , además de unos limites por el momento para evitar que la nave salga de nuestro display

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -11,16 +11,23 @@ from game.utils.constants import SPACESHIP
 # SpaceShip es una clase derivada (hija) de Sprite
 
 # spaceship tiene una "imagen"
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
 class SpaceShip(Sprite):
-    
     def __init__(self):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.image_size = (40, 60)
         self.image = pygame.transform.scale(SPACESHIP, self.image_size)
         self.image_rect = self.image.get_rect()
         self.image_rect.x = self.image_size[0]
         self.image_rect.y = self.image_size[1]
-
+    
     def update(self):
-        pass
-
-
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and self.image_rect.left > 0:  # Tecla A para moverse a la izquierda
+            self.image_rect.x -= 5
+        if keys[pygame.K_d] and self.image_rect.right < SCREEN_WIDTH:  # Tecla D para moverse a la derecha
+            self.image_rect.x += 5
+        if keys[pygame.K_w] and self.image_rect.top > 0:  # Tecla W para moverse hacia arriba
+            self.image_rect.y -= 5
+        if keys[pygame.K_s] and self.image_rect.bottom < SCREEN_HEIGHT:  # Tecla S para moverse hacia abajo
+            self.image_rect.y += 5
